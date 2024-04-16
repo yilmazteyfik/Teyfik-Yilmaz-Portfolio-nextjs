45e81dfa3ebd30d51ed96854899f649298ceebb0
yilmazteyfik <teyfik.yilmaz@bahcesehir.edu.tr>
2024-04-16 03:29:18 +0300

    add cl'k cv

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-white via-zinc-500/10 to-black">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
@@ -29,21 +29,34 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+    Teyfik Yilmaz
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+        Harnessing the power of coding to deliver tailored and unique solutions.
         </h2>
+        <a
+                    href="/pdf/TeyfikYilmazCV0324pdf.pdf"
+                    download="TeyfikYilmaz.pdf"
+                    className="text-zinc-500 hover:text-zinc-300 focus:outline-none inline-flex items-center mt-2"
+                >
+                    <svg
+                        className="w-6 h-6 mr-1"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M9 5l7 7-7 7"
+                        />
+                    </svg>
+                    Click to download my CV
+                </a>
       </div>
     </div>
   );
